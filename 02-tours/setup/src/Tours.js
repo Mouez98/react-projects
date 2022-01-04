318009c196646fee52eb33ff1b5bb8fc9b6930f3
Mouez98 <mouez.matoui@gmail.com>
2022-01-04 21:58:20 +0100

    initializing

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,23 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours , removeTour}) => {
+  //  let tour =  tours.map((tour) => {
+  //     let { price, image, name, info ,id} = tour;
+  //     return <Tour
+
+  //                key={id} />;
+  //   });
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+      {tours.map((tour) => {
+        return <Tour key={tour.id} {...tour} removeTour={removeTour}/>;
+      })}
+    </section>
+  );
 };
 
 export default Tours;
